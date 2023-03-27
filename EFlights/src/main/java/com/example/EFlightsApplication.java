221cdd8d8beaeb493b9ad2d185146d6a1f830469
elboop86 <javi86medina@hotmail.com>
2023-03-28 00:07:25 +0200

    Initial commit

---
+++ EFlights/src/main/java/com/example/EFlightsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFlightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EFlightsApplication.class, args);
+	}
+
+}
